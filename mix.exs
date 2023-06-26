70d18cdfe5100d0339205d892d948638a06b2154
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-06-26 17:14:59 +0200

    Release v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.OggPlugin.Mixfile do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.3.0"
   @github_url "https://github.com/membraneframework/membrane_ogg_plugin"
 
   def project do
@@ -37,8 +37,8 @@ defmodule Membrane.OggPlugin.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.11.0"},
-      {:membrane_common_c, "~> 0.14.0"},
+      {:membrane_core, "~> 0.12.3"},
+      {:membrane_common_c, "~> 0.15.0"},
       {:unifex, "~> 1.1"},
       # TODO: publish on hex and don't reference github
       {:membrane_ogg_format, github: "membraneframework/membrane_ogg_format"},
